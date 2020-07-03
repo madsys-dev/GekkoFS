a782c2e72bd9de1deea19f136c5fe1c0df9e2abe
Runji Wang <wangrunji0408@163.com>
2020-07-03 16:54:08 +0800

    add dependencies install script for centos

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -2,3 +2,8 @@ sudo apt install \
     cmake pkg-config uuid-dev libtool libcapstone-dev \
     libsnappy-dev zlib1g-dev libbz2-dev libzstd-dev liblz4-dev \
     libboost-filesystem-dev libboost-program-options-dev libboost-system-dev
+
+sudo yum install \
+    cmake pkg-config uuid-devel libtool capstone-devel \
+    snappy-devel zlib-devel bzip2-devel zstd-devel lz4-devel \
+    boost-devel
